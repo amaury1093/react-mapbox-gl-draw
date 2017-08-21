458df8d0207598680a0bb9f40001197df4f563c6
Amaury Martiny <amaury.martiny@gmail.com>
2017-08-21 23:25:59 +0200

    Add lint in npm test

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactMapboxGl, { FeatureGroup } from 'react-mapbox-gl';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  const Map = ReactMapboxGl({
+    accessToken: 'pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w',
+  });
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to react-mapbox-gl-draw</h2>
       </div>
-    );
-  }
-}
+      <Map
+        style="mapbox://styles/mapbox/outdoors-v9" // eslint-disable-line
+        containerStyle={{ height: '400px', width: '100vw' }}
+      >
+        {/* <DrawControl
+        controls={{ point: false, line_string: false, combine_features: false, uncombine_features: false }}
+        /> */}
+      </Map>
+    </div>
+  );
+};
 
 export default App;
