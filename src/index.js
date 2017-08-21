fb190727419f53f1119af07737089a73cd9ee244
Amaury Martiny <amaury.martiny@gmail.com>
2017-08-21 23:25:19 +0200

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -3,30 +3,6 @@ import PropTypes from 'prop-types';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 
 export default class DrawControl extends React.Component {
-  static contextTypes = {
-    map: PropTypes.object.isRequired,
-  };
-
-  static propTypes = {
-    keybindings: PropTypes.bool,
-    touchEnabled: PropTypes.bool,
-    boxSelect: PropTypes.bool,
-    clickBuffer: PropTypes.number,
-    touchBuffer: PropTypes.number,
-    controls: PropTypes.shape({
-      point: PropTypes.bool,
-      line_string: PropTypes.bool,
-      polygon: PropTypes.bool,
-      trash: PropTypes.bool,
-      combine_features: PropTypes.bool,
-      uncombine_features: PropTypes.bool,
-    }),
-    displayControlsDefault: PropTypes.bool,
-    styles: PropTypes.arrayOf(PropTypes.object),
-    modes: PropTypes.object,
-    default_mode: PropTypes.string
-  };
-
   componentWillMount() {
     const { map } = this.context;
     const Draw = new MapboxDraw(this.props);
@@ -37,3 +13,47 @@ export default class DrawControl extends React.Component {
     return null;
   }
 }
+
+DrawControl.contextTypes = {
+  map: PropTypes.object.isRequired,
+};
+
+DrawControl.propTypes = {
+  keybindings: PropTypes.bool,
+  touchEnabled: PropTypes.bool,
+  boxSelect: PropTypes.bool,
+  clickBuffer: PropTypes.number,
+  touchBuffer: PropTypes.number,
+  controls: PropTypes.shape({
+    point: PropTypes.bool,
+    line_string: PropTypes.bool,
+    polygon: PropTypes.bool,
+    trash: PropTypes.bool,
+    combine_features: PropTypes.bool,
+    uncombine_features: PropTypes.bool,
+  }),
+  displayControlsDefault: PropTypes.bool,
+  styles: PropTypes.arrayOf(PropTypes.object),
+  modes: PropTypes.object, // eslint-disable-line
+  default_mode: PropTypes.string,
+};
+
+DrawControl.defaultProps = {
+  keybindings: true,
+  touchEnabled: true,
+  boxSelect: true,
+  clickBuffer: 2,
+  touchBuffer: 25,
+  controls: {
+    point: true,
+    line_string: true,
+    polygon: true,
+    trash: true,
+    combine_features: true,
+    uncombine_features: true,
+  },
+  displayControlsDefault: true,
+  styles: [],
+  modes: {},
+  default_mode: 'simple_select',
+};
