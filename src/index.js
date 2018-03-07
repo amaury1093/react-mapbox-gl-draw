c082f4d8677bbb6d57bfd38f05864e11ea03c641
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-07 18:01:23 +0100

    Properly lint code

--- src/index.js
+++ src/index.js
@@ -3,83 +3,100 @@ import PropTypes from 'prop-types';
 import MapboxDraw from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw';
 
 export default class DrawControl extends React.Component {
-  componentWillMount() {
+  static contextTypes = {
+    map: PropTypes.object.isRequired
+  };
+
+  static defaultProps = {
+    onDrawActionable: () => {},
+    onDrawCombine: () => {},
+    onDrawCreate: () => {},
+    onDrawDelete: () => {},
+    onDrawModeChange: () => {},
+    onDrawRender: () => {},
+    onDrawSelectionChange: () => {},
+    onDrawUncombine: () => {},
+    onDrawUpdate: () => {},
+    position: 'top-left'
+  };
+
+  static propTypes = {
+    boxSelect: PropTypes.bool,
+    clickBuffer: PropTypes.number,
+    controls: PropTypes.shape({
+      point: PropTypes.bool,
+      line_string: PropTypes.bool,
+      polygon: PropTypes.bool,
+      trash: PropTypes.bool,
+      combine_features: PropTypes.bool,
+      uncombine_features: PropTypes.bool
+    }),
+    default_mode: PropTypes.string,
+    displayControlsDefault: PropTypes.bool,
+    keybindings: PropTypes.bool,
+    modes: PropTypes.object,
+    position: PropTypes.string,
+    onDrawActionable: PropTypes.func,
+    onDrawCombine: PropTypes.func,
+    onDrawCreate: PropTypes.func,
+    onDrawDelete: PropTypes.func,
+    onDrawModeChange: PropTypes.func,
+    onDrawRender: PropTypes.func,
+    onDrawSelectionChange: PropTypes.func,
+    onDrawUncombine: PropTypes.func,
+    onDrawUpdate: PropTypes.func,
+    touchBuffer: PropTypes.number,
+    touchEnabled: PropTypes.bool,
+
+    styles: PropTypes.arrayOf(PropTypes.object)
+  };
+
+  componentWillMount () {
+    const {
+      modes,
+      onDrawActionable,
+      onDrawCombine,
+      onDrawCreate,
+      onDrawDelete,
+      onDrawModeChange,
+      onDrawRender,
+      onDrawSelectionChange,
+      onDrawUncombine,
+      onDrawUpdate,
+      position
+    } = this.props;
+    const { map } = this.context;
+
     this.draw = new MapboxDraw({
       ...this.props,
       modes: {
         ...MapboxDraw.modes,
-        ...this.props.modes,
-      },
+        ...modes
+      }
     });
-    const { map } = this.context;
-    map.addControl(this.draw, this.props.position);
-    // hook draw events
-    map.on('draw.create', this.props.onDrawCreate);
-    map.on('draw.delete', this.props.onDrawDelete);
-    map.on('draw.update', this.props.onDrawUpdate);
-    map.on('draw.combine', this.props.onDrawCombine);
-    map.on('draw.uncombine', this.props.onDrawUncombine);
-    map.on('draw.selectionchange', this.props.onDrawSelectionChange);
-    map.on('draw.modechange', this.props.onDrawModeChange);
-    map.on('draw.render', this.props.onDrawRender);
-    map.on('draw.actionable', this.props.onDrawActionable);
+    map.addControl(this.draw, position);
+
+    // Hook draw events
+    map.on('draw.actionable', onDrawActionable);
+    map.on('draw.combine', onDrawCombine);
+    map.on('draw.create', onDrawCreate);
+    map.on('draw.delete', onDrawDelete);
+    map.on('draw.modechange', onDrawModeChange);
+    map.on('draw.render', onDrawRender);
+    map.on('draw.selectionchange', onDrawSelectionChange);
+    map.on('draw.uncombine', onDrawUncombine);
+    map.on('draw.update', onDrawUpdate);
   }
 
-  componentWillUnmount() {
-    if (!this.context.map || !this.context.map.getStyle()) {
+  componentWillUnmount () {
+    const { map } = this.context;
+    if (!map || !map.getStyle()) {
       return;
     }
-    this.context.map.removeControl(this.draw);
+    map.removeControl(this.draw);
   }
 
-  render() {
+  render () {
     return null;
   }
 }
-
-DrawControl.contextTypes = {
-  map: PropTypes.object.isRequired,
-};
-
-DrawControl.defaultProps = {
-  position: 'top-left',
-  onDrawCreate: () => {},
-  onDrawDelete: () => {},
-  onDrawUpdate: () => {},
-  onDrawCombine: () => {},
-  onDrawUncombine: () => {},
-  onDrawSelectionChange: () => {},
-  onDrawModeChange: () => {},
-  onDrawRender: () => {},
-  onDrawActionable: () => {},
-};
-
-DrawControl.propTypes = {
-  onDrawCreate: PropTypes.func,
-  onDrawDelete: PropTypes.func,
-  onDrawUpdate: PropTypes.func,
-  onDrawCombine: PropTypes.func,
-  onDrawUncombine: PropTypes.func,
-  onDrawSelectionChange: PropTypes.func,
-  onDrawModeChange: PropTypes.func,
-  onDrawRender: PropTypes.func,
-  onDrawActionable: PropTypes.func,
-  keybindings: PropTypes.bool,
-  touchEnabled: PropTypes.bool,
-  boxSelect: PropTypes.bool,
-  clickBuffer: PropTypes.number,
-  touchBuffer: PropTypes.number,
-  position: PropTypes.string,
-  controls: PropTypes.shape({
-    point: PropTypes.bool,
-    line_string: PropTypes.bool,
-    polygon: PropTypes.bool,
-    trash: PropTypes.bool,
-    combine_features: PropTypes.bool,
-    uncombine_features: PropTypes.bool,
-  }),
-  displayControlsDefault: PropTypes.bool,
-  styles: PropTypes.arrayOf(PropTypes.object),
-  modes: PropTypes.object, // eslint-disable-line
-  default_mode: PropTypes.string,
-};
